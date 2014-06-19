d08021843716ed9c5849a4f9ef2bf0425b5c60d1
Nicholas Mayne <Jetski5822@gmail.com>
2014-06-19 22:24:35 +0100

    Updating to work with latest code

---
+++ Filters/IHttpAuthorizationFilter.cs
@@ -0,0 +1,6 @@
+﻿using Orchard.WebApi.Filters;
+
+namespace NGM.CasClient.Filters {
+    public interface IHttpAuthorizationFilter : System.Web.Http.Filters.IAuthorizationFilter, IApiFilterProvider {
+    }
+}
--- Filters/WebApi/WebApiActionFilter.cs
+++ Filters/WebApi/WebApiActionFilter.cs
@@ -1,12 +1,9 @@
 using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Diagnostics.Contracts;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
-using Orchard.Validation;
 using Orchard.WebApi.Filters;
 
 namespace NGM.CasClient.Filters.WebApi {
@@ -21,66 +18,28 @@ namespace NGM.CasClient.Filters.WebApi {
         public virtual void OnActionExecuted(HttpActionExecutedContext actionExecutedContext) {
         }
 
-        [SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification = "We want to intercept all exceptions")]
-        Task<HttpResponseMessage> IActionFilter.ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation) {
-            if (actionContext == null) {
-                Argument.ThrowIfNull(actionContext, "actionContext");
-            }
-            if (continuation == null) {
-                Argument.ThrowIfNull(continuation, "continuation");
-            }
-
-            try {
-                OnActionExecuting(actionContext);
-            }
-            catch (Exception e) {
-                return TaskHelpers.FromError<HttpResponseMessage>(e);
-            }
+        public Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext actionContext, CancellationToken cancellationToken,
+        Func<Task<HttpResponseMessage>> continuation) {
+            OnActionExecuting(actionContext);
 
             if (actionContext.Response != null) {
-                return TaskHelpers.FromResult(actionContext.Response);
+                return Task.Factory.StartNew(() => { return actionContext.Response; });
             }
 
-            Task<HttpResponseMessage> internalTask = continuation();
-            bool calledOnActionExecuted = false;
+            HttpActionExecutedContext executedContext;
 
-            return internalTask
-                .Then(response => {
-                    calledOnActionExecuted = true;
-                    Tuple<HttpResponseMessage, Exception> result = CallOnActionExecuted(actionContext, response: response);
-                    return result.Item1 != null ? TaskHelpers.FromResult(result.Item1) : TaskHelpers.FromError<HttpResponseMessage>(result.Item2);
-                }, cancellationToken)
-                .Catch<HttpResponseMessage>(info => {
-                    // If we've already called OnActionExecuted, that means this catch is running because
-                    // OnActionExecuted threw an exception, so we just want to re-throw the exception rather
-                    // that calling OnActionExecuted again. We also need to reset the response to forget about it
-                    // since a filter threw an exception.
-                    if (calledOnActionExecuted) {
-                        actionContext.Response = null;
-                        return info.Throw();
-                    }
-
-                    Tuple<HttpResponseMessage, Exception> result = CallOnActionExecuted(actionContext, exception: info.Exception);
-                    return result.Item1 != null ? info.Handled(result.Item1) : info.Throw(result.Item2);
-                }, cancellationToken);
-        }
-
-        private Tuple<HttpResponseMessage, Exception> CallOnActionExecuted(HttpActionContext actionContext, HttpResponseMessage response = null, Exception exception = null) {
-            Contract.Assert(actionContext != null);
-            Contract.Assert(response != null || exception != null);
-
-            HttpActionExecutedContext executedContext = new HttpActionExecutedContext(actionContext, exception) { Response = response };
+            try {
+                var response = continuation();
+                executedContext = new HttpActionExecutedContext(actionContext, null) {
+                    Response = response.Result
+                };
+            }
+            catch (Exception exception) {
+                executedContext = new HttpActionExecutedContext(actionContext, exception);
+            }
 
             OnActionExecuted(executedContext);
-
-            if (executedContext.Response != null) {
-                return new Tuple<HttpResponseMessage, Exception>(executedContext.Response, null);
-            }
-            if (executedContext.Exception != null) {
-                return new Tuple<HttpResponseMessage, Exception>(null, executedContext.Exception);
-            }
-
-            throw new Exception("");// Error.InvalidOperation(SRResources.ActionFilterAttribute_MustSupplyResponseOrException, GetType().Name);
+            return Task.Factory.StartNew(() => { return executedContext.Response; });
         }
     }
 }
